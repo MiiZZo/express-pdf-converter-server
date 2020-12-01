e62099a7309ecbe7d5c7a9c1e4b77204bbd6ff6c
miizzo <miizzo.js@mail.ru>
2020-12-01 22:19:18 +0300

    update

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const bootstrap = async () => {
   try {
     const app = express();
     app.use(express.json());
-    app.use(cors());
+    app.use(cors({
+      origin: true
+    }));
 
     app.post('/', async (req, res) => {
       try {
