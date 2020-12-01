fe3cbf70ef64fc64bdf36b7cfb7a9e14c72c1f4a
miizzo <miizzo.js@mail.ru>
2020-12-01 22:34:19 +0300

    updated

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ const bootstrap = async () => {
 
     app.post('/', async (req, res) => {
       res.setHeader('Access-Control-Allow-Credentials', true)
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      // another common pattern
-      // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
+      res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
       res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
       res.setHeader(
         'Access-Control-Allow-Headers',
