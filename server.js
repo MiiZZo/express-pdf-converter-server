63d99ac0c42ab0e651c0d01102d872ac13a0ed16
miizzo <miizzo.js@mail.ru>
2020-12-01 22:09:30 +0300

    solved the problem with cors

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const bootstrap = async () => {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    
+      res.header('Access-Controll-Max-Age', '30000000000000');
+
       next();
     });
 
