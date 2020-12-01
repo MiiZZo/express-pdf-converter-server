c0e2d04eeb3fe51e5d3666c8adcd829d815f3acd
miizzo <miizzo.js@mail.ru>
2020-12-01 22:07:29 +0300

    updated

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const bootstrap = async () => {
   try {
     const app = express();
     app.use(express.json());
-
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
       next();
     });
 
